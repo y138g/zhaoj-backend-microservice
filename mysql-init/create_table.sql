ae0e878dc31f06857e82ceeff1cf2b73750a8c0e
y138g <BlueHorizon6005@gmail.com>
2025-04-22 16:45:25 +0800

    [Dev-0010] 2025.4.22 fix：修复了不可打包问题、类路径问题，同时删除了一些无用的服务

--- mysql-init/create_table.sql
+++ mysql-init/create_table.sql
@@ -1,18 +1,49 @@
 /*
  Navicat Premium Data Transfer
 
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80024 (8.0.24)
+ Source Server Version : 80041 (8.0.41)
+ Source Host           : localhost:3306
+ Source Schema         : zhaoj
 
  Target Server Type    : MySQL
- Target Server Version : 80024 (8.0.24)
+ Target Server Version : 80041 (8.0.41)
  File Encoding         : 65001
 
- Date: 27/09/2024 10:11:32
+ Date: 14/04/2025 20:35:48
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bank
+-- ----------------------------
+DROP TABLE IF EXISTS `bank`;
+CREATE TABLE `bank` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `title` varchar(1024) NOT NULL COMMENT '题库标题',
+  `userId` bigint NOT NULL COMMENT '题库创建者 id',
+  `questionNum` int DEFAULT '0' COMMENT '题库题目数量',
+  `content` text NOT NULL COMMENT '题库描述',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1911019347776761858 DEFAULT CHARSET=utf16 COMMENT='题库';
+
+-- ----------------------------
+-- Table structure for bank_question
+-- ----------------------------
+DROP TABLE IF EXISTS `bank_question`;
+CREATE TABLE `bank_question` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `bankId` bigint NOT NULL COMMENT '题库主键',
+  `questionId` bigint NOT NULL COMMENT '题目主键',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1911709980027846659 DEFAULT CHARSET=utf16 COMMENT='题库-题目';
 
 -- ----------------------------
 -- Table structure for chart
@@ -20,12 +51,12 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `chart`;
 CREATE TABLE `chart` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `goal` text COLLATE utf8mb4_unicode_ci COMMENT '分析目标',
-  `chartData` text COLLATE utf8mb4_unicode_ci COMMENT '图表数据',
-  `chartName` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '图表名称',
-  `chartType` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '图表类型',
-  `genChart` text COLLATE utf8mb4_unicode_ci COMMENT '生成的图表数据',
-  `genResult` text COLLATE utf8mb4_unicode_ci COMMENT '生成的分析结论',
+  `goal` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '分析目标',
+  `chartData` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '图表数据',
+  `chartName` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '图表名称',
+  `chartType` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '图表类型',
+  `genChart` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '生成的图表数据',
+  `genResult` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '生成的分析结论',
   `userId` bigint DEFAULT NULL COMMENT '创建用户 id',
   `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -39,9 +70,9 @@ CREATE TABLE `chart` (
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `title` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标题',
-  `content` text COLLATE utf8mb4_unicode_ci COMMENT '内容',
-  `tags` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标签列表（json 数组）',
+  `title` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标题',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '内容',
+  `tags` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标签列表（json 数组）',
   `thumbNum` int NOT NULL DEFAULT '0' COMMENT '点赞数',
   `favourNum` int NOT NULL DEFAULT '0' COMMENT '收藏数',
   `userId` bigint NOT NULL COMMENT '创建用户 id',
@@ -88,14 +119,15 @@ CREATE TABLE `post_thumb` (
 DROP TABLE IF EXISTS `question`;
 CREATE TABLE `question` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `title` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标题',
-  `content` text COLLATE utf8mb4_unicode_ci COMMENT '内容',
-  `tags` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标签列表（json 数组）',
-  `answer` text COLLATE utf8mb4_unicode_ci COMMENT '题目答案',
+  `title` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标题',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '内容',
+  `tags` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '标签列表（json 数组）',
+  `difficulty` tinyint NOT NULL COMMENT '题目难度，0-简单；1-中等；2-困难',
   `submitNum` int NOT NULL DEFAULT '0' COMMENT '题目提交数',
+  `answer` text COLLATE utf8mb4_unicode_ci COMMENT '题目答案',
   `accepted` int NOT NULL DEFAULT '0' COMMENT '题目通过数',
-  `judgeCase` text COLLATE utf8mb4_unicode_ci COMMENT '判题用例(json 数组)',
-  `judgeConfig` text COLLATE utf8mb4_unicode_ci COMMENT '判题配置(json 对象)',
+  `judgeCase` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '判题用例(json 数组)',
+  `judgeConfig` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '判题配置(json 对象)',
   `thumbNum` int NOT NULL DEFAULT '0' COMMENT '点赞数',
   `favourNum` int NOT NULL DEFAULT '0' COMMENT '收藏数',
   `userId` bigint NOT NULL COMMENT '创建用户 id',
@@ -104,7 +136,7 @@ CREATE TABLE `question` (
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`),
   KEY `idx_userId` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1805551719459389443 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='题目';
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='题目';
 
 -- ----------------------------
 -- Table structure for question_submit
@@ -112,9 +144,9 @@ CREATE TABLE `question` (
 DROP TABLE IF EXISTS `question_submit`;
 CREATE TABLE `question_submit` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `language` varchar(128) COLLATE utf8mb4_general_ci NOT NULL COMMENT '编程语言',
-  `code` text COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户提交代码',
-  `judgeInfo` text COLLATE utf8mb4_general_ci COMMENT '判题信息（json 对象）',
+  `language` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '编程语言',
+  `code` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户提交代码',
+  `judgeInfo` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT '判题信息（json 对象）',
   `status` int NOT NULL DEFAULT '0' COMMENT '判题状态（0-待判题，1-判题中，2-成功，3-失败）',
   `questionId` bigint NOT NULL COMMENT '题目id',
   `userId` bigint NOT NULL COMMENT '创建用户 id',
@@ -124,7 +156,22 @@ CREATE TABLE `question_submit` (
   PRIMARY KEY (`id`),
   KEY `idx_postId` (`questionId`),
   KEY `idx_userId` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1807787254546505730 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='题目提交';
+) ENGINE=InnoDB AUTO_INCREMENT=1910010131477741571 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='题目提交';
+
+-- ----------------------------
+-- Table structure for ranking_list
+-- ----------------------------
+DROP TABLE IF EXISTS `ranking_list`;
+CREATE TABLE `ranking_list` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `bankId` varchar(1024) NOT NULL COMMENT '题库',
+  `userId` bigint NOT NULL COMMENT '排行榜用户用户 id',
+  `finishNum` bigint NOT NULL COMMENT '该用户完成数',
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COMMENT='排行榜';
 
 -- ----------------------------
 -- Table structure for user
@@ -132,19 +179,19 @@ CREATE TABLE `question_submit` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `userAccount` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '账号',
-  `userPassword` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
-  `unionId` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '微信开放平台id',
-  `mpOpenId` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '公众号openId',
-  `userName` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户昵称',
-  `userAvatar` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户头像',
-  `userProfile` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户简介',
-  `userRole` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user' COMMENT '用户角色：user/admin/ban',
+  `userAccount` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '账号',
+  `userPassword` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
+  `unionId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '微信开放平台id',
+  `mpOpenId` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '公众号openId',
+  `userName` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户昵称',
+  `userAvatar` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户头像',
+  `userProfile` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户简介',
+  `userRole` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user' COMMENT '用户角色：user/admin/ban',
   `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `isDelete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`),
   KEY `idx_unionId` (`unionId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1807346767142408195 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户';
+) ENGINE=InnoDB AUTO_INCREMENT=1910549428417134594 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户';
 
 SET FOREIGN_KEY_CHECKS = 1;
